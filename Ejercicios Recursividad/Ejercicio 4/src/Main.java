113446d174260e29b3dae5f5d119d23870ee5bd5
Jaime-Olcina <jaime.olcinamagaz@usp.ceu.es>
2026-06-16 18:34:51 +0200

    Commit 16/06 4

---
+++ Ejercicios Recursividad/Ejercicio 4/src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int elevado = Elevar.Potencia(2,3)
+
+                System.out.println("El numero que da al elevar " + i + " a " + j + " es " + elevado);
+        }
+    }
+}
