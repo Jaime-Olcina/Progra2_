9a3a1429a60c5e1d898a076c7c1dc30bb87d1d58
Jaime-Olcina <jaime.olcinamagaz@usp.ceu.es>
2026-06-19 11:35:08 +0200

    Commir 19/06

--- Blackboard/Practica (Contenido Adicional)/15. ListaEnlazada/src/App.java
+++ Blackboard/Practica (Contenido Adicional)/15. ListaEnlazada/src/App.java
@@ -4,12 +4,21 @@ public class App {
     public static void main(String[] args) throws Exception {
         
         ListaEnlazada ll = new ListaEnlazada();
-        ll.anyadir(0);
         ll.anyadir(1);
+        ll.anyadir(1);
+        ll.anyadir(17);
+        ll.anyadir(4);
+        ll.anyadir(5);
+        ll.anyadir(1,6);
+        ll.anyadir(1);
+        ll.anyadir(23);
+        ll.anyadir(0);
+        ll.anyadir(6);
+        ll.anyadir(7);
         System.out.println(ll.acceder(0));
+
+        for (int i = 0; i < 10; i++) {
+            System.out.println(ll.acceder(i));
+        }
     }
-
-
-
-
 }
