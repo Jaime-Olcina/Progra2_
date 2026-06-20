299cf63b4923b64d7738ad12e94fe60ece1d5d5e
Jaime-Olcina <jaime.olcinamagaz@usp.ceu.es>
2026-06-20 20:20:34 +0200

    Commit 20/06

--- Ejercicios Recursividad/Ejercicio 8 (Func Aux)/src/Palindromo.java
+++ Ejercicios Recursividad/Ejercicio 8 (Func Aux)/src/Palindromo.java
@@ -1,24 +1,6 @@
 public class Palindromo {
     public static boolean Lectura(String Palabra) {
-        return LecturaAux(Palabra, 0, Palabra.length() - 1);
-    }
 
-    private static boolean LecturaAux(String Palabra, int indice1, int indice2){
-       if(Palabra.length()%2==0){
-            if(Palabra.charAt(indice1)!=Palabra.charAt(indice2)){
-                return false;
-            }
-                if(indice1+1==indice2){
-                    return true;
-                }
-                return LecturaAux(Palabra,indice1+1,indice2-1);
-        }
-            if(Palabra.charAt(indice1)!=Palabra.charAt(indice2)){
-                return false;
-        }
-            if(indice1==indice2){
-                return true;
-        }
-            return LecturaAux(Palabra,indice1+1,indice2-1);
+        return PalindromoAux.LecturaAux(Palabra, 0, Palabra.length() - 1);
     }
 }
--- Ejercicios Recursividad/Ejercicio 8 (Func Aux)/src/PalindromoAux.java
+++ Ejercicios Recursividad/Ejercicio 8 (Func Aux)/src/PalindromoAux.java
@@ -1,1 +1,20 @@
-
+public class PalindromoAux{
+    public static boolean LecturaAux(String Palabra, int indice1, int indice2){
+        if(Palabra.length()%2==0){
+            if(Palabra.charAt(indice1)!=Palabra.charAt(indice2)){
+                return false;
+            }
+            if(indice1+1==indice2){
+                return true;
+            }
+            return LecturaAux(Palabra,indice1+1,indice2-1);
+        }
+        if(Palabra.charAt(indice1)!=Palabra.charAt(indice2)){
+            return false;
+        }
+        if(indice1==indice2){
+            return true;
+        }
+        return LecturaAux(Palabra,indice1+1,indice2-1);
+    }
+}
